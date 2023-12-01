a02bb9daec29f05aaf6553bb608b9444bb3524da
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2023-12-01 16:41:59 +0100

    solve challenge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import React from "react";
 import "./styles.css";
 
+function Button({ color, isDisabled, text }) {
+  const buttonStyle = {
+    backgroundColor: color.backgroundColor,
+  };
+
+  return (
+    <div>
+      <button type="button" style={buttonStyle} disabled={isDisabled}>
+        {text}
+      </button>
+    </div>
+  );
+}
+
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Button
+        color={{ backgroundColor: "#FF0000" }}
+        isDisabled={true}
+        text="Don't click me"
+      />
+      <Button
+        color={{ backgroundColor: "#00A36C" }}
+        isDisabled={false}
+        text="Click me"
+      />
+    </div>
+  );
 }
